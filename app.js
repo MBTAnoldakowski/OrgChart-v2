eeea716cad35b2ac41ac99f84b7ed6b275757a23
MBTAnoldakowski <31895313+MBTAnoldakowski@users.noreply.github.com>
2017-09-29 09:57:20 -0400

    Removed mongo dependencies

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
 console.log("SERVER SIDE!");
 var app = require('express'); // creates an instance of the express lib
-//var myModule = angular.module('OrgApp', []);
-var mongoose = require('mongoose');
-var underscore = require('underscore');
-var connectionString = 'mongodb://localhost/BUDGET-DATA';
-if (process.env.MLAB_USERNAME_WEBDEV) {
-    console.log("on heroku!");
-    connectionString = process.env.MLAB_USERNAME_WEBDEV + ":" +
-        process.env.MLAB_PASSWORD_WEBDEV + "@ds135444.mlab.com:35444/heroku_829kjs4t"
-}
-
-if (process.env.MLAB_USERNAME) { // check if running remotely
-    connectionString = process.env.MLAB_USERNAME_WEBDEV + ":" +
-        process.env.MLAB_PASSWORD_WEBDEV + "@ds135444.mlab.com:35444/heroku_829kjs4t"
-}
-console.log("connecting with: " + connectionString);
-
-mongoose.connect(connectionString);
 
